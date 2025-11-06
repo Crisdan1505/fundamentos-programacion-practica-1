22e2391c34eccd1cf2694a12ef35bea3f434b433
Crisdan1505 <cristopher.euan@tecdesoftware.edu.mx>
2025-11-05 21:33:37 -0600

    ejercicio 3.4 correcto

--- ejercicios.js
+++ ejercicios.js
@@ -191,8 +191,13 @@ function tablaMultiplicar(numero) {
  */
 function numerosPares(n) {
   // TODO: Crea un array con todos los nÃºmeros pares hasta n
-  const pares = [];
-  return pares;
+  const pares = []; 
+    for (let i = 2; i <= n; i++) {
+        if (i % 2 === 0) {
+            pares.push(i);
+        }
+    }
+    return pares;
 }
 
 // ============================================
